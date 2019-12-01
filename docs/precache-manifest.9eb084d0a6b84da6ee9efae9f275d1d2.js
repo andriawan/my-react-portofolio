60d11dd29ed0a147167b2cce7fda278cf6e91498
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-12-01 09:32:45 +0700

    first build react.andriawan.com

---
+++ docs/precache-manifest.9eb084d0a6b84da6ee9efae9f275d1d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e1c9f20acd42ec9d37e0b81990afaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d24b536fd8d3ccf642",
+    "url": "/static/css/andriawan.main.css"
+  },
+  {
+    "revision": "a522864e2563cccf9dc9",
+    "url": "/static/js/andriawan.runtime-main.js"
+  },
+  {
+    "revision": "5e7240a7e5896eb7252c",
+    "url": "/static/js/js/andriawan.2.js"
+  },
+  {
+    "revision": "a4d24b536fd8d3ccf642",
+    "url": "/static/js/js/andriawan.main.js"
+  }
+]);
